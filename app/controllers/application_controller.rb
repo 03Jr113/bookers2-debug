9cfb6a704687e04993e6a4d98fa70642f910765c
RemiEGUCHI <110374856+03Jr113@users.noreply.github.com>
2022-08-25 00:55:47 +0000

    [Finish]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,20 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:new_user_registration]
+  
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def after_sign_in_path_for(resource)
-    root_path
+    user_path(current_user)
   end
 
   def after_sign_out_path_for(resource)
     root_path
+  end
+  
+  def after_sign_up_path_for(resource)
+    user_path(current_user)
   end
 
   def configure_permitted_parameters
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,15 @@
 class BooksController < ApplicationController
+  
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @book = Book.find(params[:id])
+    @new_book = Book.new
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -24,6 +28,7 @@ class BooksController < ApplicationController
   end
 
   def update
+    @new_book = Book.new
     @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
@@ -32,15 +37,22 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
+  
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to books_path unless @user == current_user
+  end
+  
 end
